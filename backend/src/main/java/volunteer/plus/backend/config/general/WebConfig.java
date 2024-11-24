15f12603b320491da1f5f12b3047190fb8861c32
ArtemAbrams <barishartem@gmail.com>
2024-11-24 23:02:01 +0200

    Add cors config

---
+++ backend/src/main/java/volunteer/plus/backend/config/general/WebConfig.java
@@ -0,0 +1,25 @@
+package volunteer.plus.backend.config.general;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Дозволяємо CORS для всіх маршрутів
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
