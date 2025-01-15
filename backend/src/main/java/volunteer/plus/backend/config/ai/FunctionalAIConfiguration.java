fabb1e10813be51fd0bfa00302a486478f937a98
Maksym Khodakov <48530830+maksymkhodakov@users.noreply.github.com>
2025-01-15 23:05:16 +0200

    enhance functional ai call (#15)

--- backend/src/main/java/volunteer/plus/backend/config/ai/FunctionalAIConfiguration.java
+++ backend/src/main/java/volunteer/plus/backend/config/ai/FunctionalAIConfiguration.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
+import volunteer.plus.backend.domain.dto.BrigadeCreationRequestDTO;
 import volunteer.plus.backend.domain.dto.BrigadeDTO;
 import volunteer.plus.backend.domain.dto.BrigadeNameDTO;
 import volunteer.plus.backend.service.general.BrigadeService;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Configuration
@@ -23,4 +25,24 @@ public class FunctionalAIConfiguration {
             return brigade == null ? new BrigadeDTO() : brigade;
         };
     }
+
+    @Bean
+    @Description(
+            """
+            Provides a function to create or update a list of army brigades based on the given
+            {@link BrigadeCreationRequestDTO}. Each brigade in the request contains properties such as:
+            - regimentCode (mandatory): Unique identifier for the regiment.
+            - name: Official name of the brigade.
+            - branch: Military branch (e.g., Army, Navy).
+            - role: Role or function within the military.
+            - partOf: The larger unit or formation the brigade belongs to.
+            - websiteLink: URL to the brigade's official website.
+            - currentCommander: Name or identifier of the current brigade commander.
+            - description: Additional details about the brigade.
+            """
+    )
+    public Function<BrigadeCreationRequestDTO, List<BrigadeDTO>> createOrUpdateArmyBrigade() {
+        return brigadeService::createOrUpdate;
+    }
+
 }
