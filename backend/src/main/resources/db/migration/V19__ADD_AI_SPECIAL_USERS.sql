8e95a79d7acb412ae22a5166b49ec9f4412fa790
Jovakinn <max05012004@gmail.com>
2025-03-30 23:03:59 +0300

    SCRUM add ai users chat connection

---
+++ backend/src/main/resources/db/migration/V19__ADD_AI_SPECIAL_USERS.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (email, first_name, last_name, enabled, account_non_expired, account_non_locked, credentials_non_expired)
+VALUES ('ollamaai@test.com', 'ollama', 'ollama', 1, 1, 1, 1),
+       ('openai@test.com', 'openai', 'openai', 1, 1, 1, 1);
