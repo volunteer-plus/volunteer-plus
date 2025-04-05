a84bbc829b3d780defbdb34adcff36e119129cea
Jovakinn <max05012004@gmail.com>
2025-04-05 22:12:10 +0300

    SCRUM add MCP server configs

---
+++ backend/src/main/java/volunteer/plus/backend/config/ai/ToolMCPConfig.java
@@ -0,0 +1,27 @@
+package volunteer.plus.backend.config.ai;
+
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import volunteer.plus.backend.service.ai.tools.AIAgentPattern;
+import volunteer.plus.backend.service.ai.tools.AIMilitaryTools;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Configuration
+public class ToolMCPConfig {
+
+    @Bean
+    public List<ToolCallback> tools(final AIMilitaryTools aiMilitaryTools,
+                                    final List<AIAgentPattern> aiAgentPatterns) {
+        return Stream.concat(Stream.of(aiMilitaryTools), aiAgentPatterns.stream())
+                .map(ToolCallbacks::from)
+                .map(Arrays::asList)
+                .flatMap(Collection::stream)
+                .toList();
+    }
+}
