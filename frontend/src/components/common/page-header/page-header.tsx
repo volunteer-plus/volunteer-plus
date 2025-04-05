9faa92530728f6f8bf3ac7225f1075ccdae1d0af
Denys Shevel <denys.shevel16@gmail.com>
2025-04-05 18:42:35 +0300

    Make homepage header show user

--- frontend/src/components/common/page-header/page-header.tsx
+++ frontend/src/components/common/page-header/page-header.tsx
@@ -1,28 +1,36 @@
 import { Link } from 'react-router-dom';
-import { ButtonBase } from '@/components/common';
+import { ButtonBase, SessionUserBlock } from '@/components/common';
 
 import Logo from '@/assets/logo.svg?react';
 
 import styles from './styles.module.scss';
+import { useAppSelector } from '@/hooks/store';
 
 const PageHeader: React.FC = () => {
+  const userState = useAppSelector((state) => state.user);
+
   return (
     <header className={styles.header}>
       <Link to='/'>
         <Logo />
       </Link>
-      <div className={styles.actions}>
-        <ButtonBase
-          colorSchema='gray'
-          variant='outlined'
-          elementType={Link}
-          elementProps={{ to: '/sign-in', children: 'Увійти' }}
-        />
-        <ButtonBase
-          elementType={Link}
-          elementProps={{ to: '/sign-up', children: 'Створити профіль' }}
-        />
-      </div>
+
+      {userState.user ? (
+        <SessionUserBlock />
+      ) : (
+        <div className={styles.actions}>
+          <ButtonBase
+            colorSchema='gray'
+            variant='outlined'
+            elementType={Link}
+            elementProps={{ to: '/sign-in', children: 'Увійти' }}
+          />
+          <ButtonBase
+            elementType={Link}
+            elementProps={{ to: '/sign-up', children: 'Створити профіль' }}
+          />
+        </div>
+      )}
     </header>
   );
 };
