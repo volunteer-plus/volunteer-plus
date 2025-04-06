986d6cdddbfb274a4f6f6847b2005e4bce0537ca
Jovakinn <max05012004@gmail.com>
2025-04-06 15:34:09 +0300

    SCRUM remove tools from ai agents

--- backend/src/main/java/volunteer/plus/backend/service/ai/tools/impl/OrchestratorWorkers.java
+++ backend/src/main/java/volunteer/plus/backend/service/ai/tools/impl/OrchestratorWorkers.java
@@ -2,8 +2,6 @@ package volunteer.plus.backend.service.ai.tools.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 import volunteer.plus.backend.domain.dto.ai.agent.FinalResponse;
 import volunteer.plus.backend.domain.dto.ai.agent.OrchestratorResponse;
@@ -21,16 +19,10 @@ public class OrchestratorWorkers implements AIAgentPattern {
         return AIAgentPatternType.ORCHESTRATOR_WORKERS;
     }
 
-    @Tool(name = "patternOrchestrator", description = """
-        This tool coordinates the orchestration of worker tasks based on an initial task description.
-        The tool first sends the taskDescription to the orchestrator prompt, which returns an analysis and a list of tasks.
-        Then, each task is sent to the worker prompt to generate a corresponding worker response.
-        Finally, the tool aggregates the orchestrator's analysis and the worker responses into a FinalResponse.
-        """)
-    public static FinalResponse process(@ToolParam(description = "A String describing the original task or problem.") final String taskDescription,
-                                        @ToolParam(description = "An instance of ChatClient for interacting with the AI chat engine.") final ChatClient chatClient,
-                                        @ToolParam(description = "A String prompt that instructs the orchestrator to analyze the original task and generate a list of worker tasks, each defined by a type and description.") final String orchestratorPrompt,
-                                        @ToolParam(description = "A String prompt that instructs individual workers to execute their assigned tasks, using the original task, task type, and task description.") final String workerPrompt) {
+    public static FinalResponse process(final String taskDescription,
+                                        final ChatClient chatClient,
+                                        final String orchestratorPrompt,
+                                        final String workerPrompt) {
         final OrchestratorResponse orchestratorResponse = chatClient.prompt()
                 .user(u -> u.text(orchestratorPrompt).param("task", taskDescription))
                 .call()
