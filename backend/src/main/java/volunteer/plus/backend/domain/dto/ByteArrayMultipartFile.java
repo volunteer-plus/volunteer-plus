731381b514757564fc8f0cd5da0d944673f1fa69
Maksym Khodakov <48530830+maksymkhodakov@users.noreply.github.com>
2025-04-10 01:30:57 +0300

    SCRUM add logo ai news feed generation (#59)

---
+++ backend/src/main/java/volunteer/plus/backend/domain/dto/ByteArrayMultipartFile.java
@@ -0,0 +1,68 @@
+package volunteer.plus.backend.domain.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class ByteArrayMultipartFile implements MultipartFile {
+
+    private final String name;
+    private final String originalFilename;
+    private final String contentType;
+    private final byte[] content;
+
+    public ByteArrayMultipartFile(final String name,
+                                  final String originalFilename,
+                                  final String contentType,
+                                  final byte[] content) {
+        this.name = name;
+        this.originalFilename = originalFilename;
+        this.contentType = contentType;
+        this.content = content;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return this.originalFilename;
+    }
+
+    @Override
+    public String getContentType() {
+        return this.contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (this.content == null || this.content.length == 0);
+    }
+
+    @Override
+    public long getSize() {
+        return this.content.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return this.content;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(this.content);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        Files.write(dest.toPath(), this.content);
+    }
+}
+
