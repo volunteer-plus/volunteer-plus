749684354b1cca8d7a3bf294471c5b552a3f05a7
Jovakinn <max05012004@gmail.com>
2025-04-09 23:34:31 +0300

    SCRUM add news feed attachment

---
+++ backend/src/main/java/volunteer/plus/backend/domain/dto/NewsFeedAttachmentDTO.java
@@ -0,0 +1,22 @@
+package volunteer.plus.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsFeedAttachmentDTO {
+    private Long id;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    private byte[] content;
+    private String s3Link;
+    private String filename;
+    private boolean logo;
+}
